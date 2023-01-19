ae52cc748fe776a7d1fd24bcfc3ff2049a718746
haroon-ch <haroonchlearn2earn@gmail.com>
2023-01-18 16:01:07 -0800

    image-upload update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,21 @@ import { AppRoutingModule } from './app-routing.module';
 
 // validation
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 @NgModule({
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA,
     NO_ERRORS_SCHEMA
   ],
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,  FormsModule,
-    ReactiveFormsModule],
+  imports: [BrowserModule, IonicModule.forRoot(),
+     AppRoutingModule,  
+    FormsModule, 
+
+    ReactiveFormsModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
